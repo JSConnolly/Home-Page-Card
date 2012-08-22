63dd50284b43ab17fddbf5421b281da38e4e3e79
Ryan Walton <ryang24@gmail.com>
2012-08-21 21:34:07 -0700

    v1 homepage

---
+++ HomePageApp/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls import *
+from HomePageApp.views import *
+import settings
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+						url(r'^$', 'HomePageApp.views.home_page', name='home_page'),
+    # Examples:
+    # url(r'^$', 'HomePageApp.views.home', name='home'),
+    # url(r'^HomePageApp/', include('HomePageApp.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
+urlpatterns += patterns('',
+                        (r'^HomePageApp/static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+)
---
+++ HomePageApp/views.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+views.py
+
+Created by Ryan Walton on 2012-08-21.
+Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+"""
+
+from django.shortcuts import render_to_response
+from django.template.loader import get_template
+
+def home_page(request):
+    return render_to_response('home_page.html', )
+
